78bfb5ce6421d366b67a3a38a35d18f740829e66
Nandan kumar <kumarnandan551@gmail.com>
2025-02-03 08:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 const getSumBtn = document.createElement("button");
+
 getSumBtn.append("Get Total Price");
+
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+
+    const prices = document.querySelectorAll('.price');
+
+    let total = 0;
+
+    prices.forEach(price => {
+
+        total += parseInt(price.textContent);
+
+    });
+
+    let totalRow = document.getElementById('ans');
+
+    
+
+    if (!totalRow) {
+
+        totalRow = document.createElement('tr');
+
+        totalRow.setAttribute('id', 'ans');
+
+        const totalCell = document.createElement('td');
+
+        totalCell.setAttribute('colspan', '2');
+
+        totalCell.textContent = 'Total Price: Rs ' + total;
+
+        totalRow.appendChild(totalCell);
+
+        document.querySelector('table').appendChild(totalRow);
+
+    } else {
+
+        totalRow.querySelector('td').textContent = 'Total Price: Rs ' + total;
+
+    }
+
 };
 
-getSumBtn.addEventListener("click", getSum);
-
+getSumBtn.addEventListener("click", getSum);
